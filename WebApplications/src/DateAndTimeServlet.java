1e50384ca67830d5b8af25a81427f078aa113fba
satyac11 <c.satya913@gmail.com>
2018-09-13 12:12:54 +0530

    Web Resources created

---
+++ WebApplications/src/DateAndTimeServlet.java
@@ -0,0 +1,33 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet implementation class DateAndServlet
+ */
+
+public class DateAndTimeServlet extends GenericServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		res.setContentType("text/html");
+		PrintWriter pw = res.getWriter();
+		Date date = new Date();
+		pw.println("Current Date is: "+date.toString());
+		pw.close();
+	}
+	
+}
